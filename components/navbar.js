d326606953cdb0dffc0d3a9c516ae1448b9f2229
Shashijass <shashi16ic14@gmail.com>
2021-09-13 15:39:35 +0530

    6th commit

--- components/navbar.js
+++ components/navbar.js
@@ -2,8 +2,8 @@ function navbar() {
   return ` <div id="navbar">
         <h3><a href="/home.html">Home</a></h3>
         <div id="options">
-            <h3><a href="/sportd.html">Sports</a></h3>
-            <h3><a href="/business.html">Business</a></h3>
+            <h3><a href="/serch.html">Sports</a></h3>
+            <h3><a href="/news.html">Business</a></h3>
             <h3><a href="/">Register</a></h3>
             <h3><a href="/">Login</a></h3>
         </div>
